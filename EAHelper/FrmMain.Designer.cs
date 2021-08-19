34c83e7a7091d2088483881e7f69e1f1deccc473
TimAndy <xuchonglei@126.com>
2021-08-19 19:29:45 +0800

    提交代码

---
+++ EAHelper/FrmMain.Designer.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+
+namespace EAHelper
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon) (resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.menuClose});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(101, 26);
+            // 
+            // menuClose
+            // 
+            this.menuClose.Name = "menuClose";
+            this.menuClose.Size = new System.Drawing.Size(100, 22);
+            this.menuClose.Text = "退出";
+            this.menuClose.Click += new System.EventHandler(this.menuClose_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 50;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 78);
+            this.Name = "FrmMain";
+            this.Text = "主窗口";
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ToolStripMenuItem menuClose;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+
+        #endregion
+    }
+}
---
+++ EAHelper/FrmMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace EAHelper
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void menuClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            IntPtr hwndDialog = UnsafeNativeMethods.FindWindow("#32770", "Unsaved Property Changes");
+            if (hwndDialog == IntPtr.Zero)
+                return;
+
+            IntPtr hwndYes = UnsafeNativeMethods.FindWindowEx(hwndDialog, IntPtr.Zero, "Button", "Yes");
+            if (hwndYes == IntPtr.Zero)
+                return;
+            Util.SendMouseClick(hwndYes, Point.Empty);
+        }
+    }
+}
